0b5451cc7914874048ee48e4f2afdf9d1e21c187
Robert Butler <robertbutler@protonmail.com>
2022-02-28 23:27:51 +0000

    feat: add header and responsive stylesheets

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,21 +2,19 @@ import '../styles/header.scss';
 import { ReactComponent as Background } from '../assets/bg-pattern-1.svg';
 import { ReactComponent as Logo } from '../assets/logo.svg';
 
-export default function Brand() {
+export default function Header() {
   return (
-    <section>
-      <article>
-        <Logo />
-        {/* <Background /> */}
-      </article>
-      <article>
-        <h2>We make your music sound extraordinary.</h2>
-        <p>
+    <section className='h-wrapper'>
+      <article className='h-text'>
+        <Logo className='h-logo' />
+        <h2 className='h-title'>We make your music sound extraordinary.</h2>
+        <p className='h-body'>
           A system audio equalizer specifically designed for Android and iOS. Freely tune
           the way your music sounds with a professional grade parametric EQ & volume
           mixer. Control bass, mids, treble, gain control, reverb, and more!
         </p>
       </article>
+      <Background className='h-background' />
     </section>
   );
 }
